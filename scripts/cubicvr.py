8af40542fe83f7a84faa7ab75718e8345d82364b
takahashi <takahashi@e-frontier.co.jp>
2011-08-01 19:36:00 +0900

    v0.4.0 NEW Cubic Panorama VR スクリプトを追加.
    NEW Cubic Panorama VR スクリプトを追加.
    FIX ObjectVR: iOS touchendが処理されていなかった問題を修正.

--- scripts/cubicvr.py
+++ scripts/cubicvr.py
@@ -1,7 +1,7 @@
 #
 # -*- coding: utf-8 -*-
-# @title \en Export Object VR HTML \enden
-# @title \ja Object VR HTML エクスポート \endja
+# @title \en Export Cubic VR HTML \enden
+# @title \ja Cubic VR HTML エクスポート \endja
 # @description \en \enden
 # @description \ja \endja
 # @version 0.4.0
@@ -17,13 +17,13 @@ except ImportError, e:
 	print _('error_import_numpy_failed')
 	raise
 
-SCRIPT_UUID = "c50b3d49-6573-447c-96d2-edcf903f6bf8"
+SCRIPT_UUID = "438D29FB-1125-4D74-8BE1-8AB594F8D466"
 
 class Settings:
-	number_of_col = 30
-	number_of_row = 19
-	render_hemisphere = False
-	makefolder = False
+	fov_def = 60.0
+	fov_max = 120.0
+	fov_min = 5.0
+	makefolder = True
 	output_path = None
 	extension = 0
 settings = Settings
@@ -62,16 +62,17 @@ def get_lang ():
 def get_text (text):
 	texts = {
 		'output_folder':{'ja':'出力先フォルダ', 'en':'Output Folder'},
-		'number_of_col':{'ja':'水平方向の分割数', 'en':'Number of columns (Pan)'},
-		'number_of_row':{'ja':'垂直方向の分割数', 'en':'Number of rows (Tilt)'},
-		'render_hemisphere':{'ja':'上半球のみレンダリング', 'en':'Render top hemisphere only'},
+		'fov':{'ja':'画角', 'en':'Field of view'},
+		'fov_def':{'ja':'初期値', 'en':'Default'},
+		'fov_max':{'ja':'最大', 'en':'Max'},
+		'fov_min':{'ja':'最小', 'en':'Min'},
 		'make_scenename_subfolder':{'ja':'出力先フォルダにシーン名のサブフォルダを作成', 'en':'Make scene file name subfolder in the output folder.'},
 		'error_output_folder_not_found':{'ja':'エラー: 出力先フォルダを指定してください.', 'en':'Error: Output folder not found.'},
-		'error_invalid_number_of_col':{'ja':'エラー: 分割数を指定してください.', 'en':'Error: Invalid number of col.'},
+		'error_invalid_fov':{'ja':'エラー: 画角の値が不正です.', 'en':'Error: Invalid field of view value.'},
 		'error_import_numpy_failed':{'ja':'エラー: numpyがインストールされていません。Shadeのアップデートを行ってください.', 'en':'Error: import numpy failed.'},
 		'done':{'ja':'完了', 'en':'Done'},
 		'canceled':{'ja':'キャンセル', 'en':'Canceled'},
-		'objectvr_option':{'ja':'Object VR オプション', 'en':'Object VR'},
+		'cubicvr_option':{'ja':'Cubic VR オプション', 'en':'Cubic VR'},
 		'output_option':{'ja':'出力オプション', 'en':'Output'},
 		'untitled':{'ja':'名称未設定', 'en':'Untitled'},
 		'extensions':{'ja':'出力フォーマット/JPEG(.jpg)/PNG(.png)', 'en':'Output Format/JPEG(.jpg)/PNG(.png)'}
@@ -94,12 +95,12 @@ def get_default_path ():
 def open_option_dialog ():
 	global settings
 	dialog = xshade.create_dialog_with_uuid(SCRIPT_UUID)
-	# Objectvr option
-	dialog.begin_group(_('objectvr_option'))
-	col_id = dialog.append_int(_('number_of_col'), '')
-	row_id = dialog.append_int(_('number_of_row'), '')
-	hemisphere_id = dialog.append_bool(_('render_hemisphere'))
-	dialog.end_group()
+	# Cubicvr option
+	#dialog.begin_group(_('fov'))
+	#fov_def_id = dialog.append_float(_('fov_def'), '')
+	#fov_max_id = dialog.append_float(_('fov_max'), '')
+	#fov_min_id = dialog.append_float(_('fov_min'), '')
+	#dialog.end_group()
 	# Output option
 	dialog.begin_group(_('output_option'))
 	path_id = dialog.append_path(_('output_folder'))
@@ -107,26 +108,25 @@ def open_option_dialog ():
 	extension_id = dialog.append_selection(_('extensions'))
 	dialog.end_group()
 	# set_value
-	if settings.output_path == None: dialog.set_value(path_id, get_default_path())
-	dialog.set_value(col_id, settings.number_of_col)
-	dialog.set_value(row_id, settings.number_of_row)
-	dialog.set_value(hemisphere_id, settings.render_hemisphere)
+	if not settings.output_path: dialog.set_value(path_id, get_default_path())
+	#dialog.set_value(fov_def_id, settings.fov_def)
+	#dialog.set_value(fov_max_id, settings.fov_max)
+	#dialog.set_value(fov_min_id, settings.fov_min)
 	dialog.set_value(makefolder_id, settings.makefolder)
 	dialog.set_value(extension_id, settings.extension)
 	# set_default_value
 	dialog.set_default_value(path_id, get_default_path())
-	dialog.set_default_value(col_id, defaults.number_of_col)
-	dialog.set_default_value(row_id, defaults.number_of_row)
-	dialog.set_default_value(hemisphere_id, defaults.render_hemisphere)
+	#dialog.set_default_value(fov_def_id, defaults.fov_def)
+	#dialog.set_default_value(fov_max_id, defaults.fov_max)
+	#dialog.set_default_value(fov_min_id, defaults.fov_min)
 	dialog.set_default_value(makefolder_id, defaults.makefolder)
 	dialog.set_default_value(extension_id, defaults.extension)
-	# dialog option
 	dialog.append_default_button()
-	if not dialog.ask('Object VR'):
+	if not dialog.ask('Cubic VR'):
 		return False
-	settings.number_of_col = dialog.get_value(col_id)
-	settings.number_of_row = dialog.get_value(row_id)
-	settings.render_hemisphere = dialog.get_value(hemisphere_id)
+	#settings.fov_def = dialog.get_value(fov_def_id)
+	#settings.fov_max = dialog.get_value(fov_max_id)
+	#settings.fov_min = dialog.get_value(fov_min_id)
 	settings.output_path = decode(dialog.get_value(path_id))
 	settings.makefolder = dialog.get_value(makefolder_id)
 	settings.extension = dialog.get_value(extension_id)
@@ -136,8 +136,8 @@ def open_option_dialog ():
 			print _('error_output_folder_not_found')
 			return False
 		os.mkdir(settings.output_path)
-	if settings.number_of_col <= 0:
-		print _('error_invalid_number_of_col')
+	if settings.fov_min < 0 or settings.fov_def < settings.fov_min or settings.fov_max < settings.fov_min:
+		print _('error_invalid_fov')
 		return False
 	return True
 
@@ -177,6 +177,9 @@ def rotate_ (d, c, s):
 def rotate (target, axis, r):
 	return translate(-target) * rotate_(axis, numpy.cos(r), numpy.sin(r)) * translate(target)
 
+def vec3 (v3):
+	return numpy.array(v3)
+
 def vec4 (v3):
 	return numpy.array(list(v3) + [0.0])
 
@@ -214,53 +217,65 @@ def encode (s):
 
 def start_rendering (scene, file_path):
 	global renderinfo
-	renderinfo.image_size = scene.rendering.image_size
 	animation_settings = scene.animation_settings
 	# レンダリング前の設定を保存.
+	saved_image_size = scene.rendering.image_size
 	saved_step = animation_settings.step
 	saved_starting_frame = animation_settings.starting_frame
 	saved_ending_frame = animation_settings.ending_frame
 	saved_object_movie_mode = animation_settings.object_movie_mode
-	saved_eye = scene.camera.eye
+	saved_target = scene.camera.target
+	saved_zoom = scene.camera.zoom
+	saved_bank = scene.camera.bank
+	saved_camera_mode = scene.camera.camera_mode
 	saved_active_shapes = scene.active_shapes
 	scene.select_all()
-	saved_sequence_value = scene.sequence_value
 	try:
-		total_frames = settings.number_of_row * settings.number_of_col
+		eye = vec3(scene.camera.eye)
+		target = vec3(scene.camera.target)
+		d = distance(eye - target)
+		rot = scene.camera.bank
+		pos = (
+			( 0, 0, 1),
+			(-1, 0, 0),
+			( 0, 0,-1),
+			( 1, 0, 0),
+			( 0, 1, 0),
+			( 0,-1, 0)
+		)
+		total_frames = 6
 		animation_settings.step = 1
 		animation_settings.starting_frame = 0
 		animation_settings.ending_frame = total_frames - 1
+		image_size = scene.rendering.image_size
+		scene.rendering.image_size = (max(image_size), max(image_size))
+		renderinfo.image_size = scene.rendering.image_size
 		renderinfo.start_time = time.time()
 		scene.rendering.start_animation(encode(file_path))
-		#rad = (2.0 * math.pi) / number_of_col
-		row = (settings.number_of_row * 2 - 1) if settings.render_hemisphere else settings.number_of_row
-		rotations = row * settings.number_of_col - 1
-		# 視線が水平になるように視点を移動する.
-		eye = vec4(scene.camera.eye)
-		target = vec4(scene.camera.target)
-		d = distance(eye - target) * 2.0
-		eye[1] = target[1]
-		eye = target + normalize(eye - target) * d
+		scene.camera.zoom = 18.0 # fov 90 deg.
 		for i in range(total_frames):
 			print '%(i)d / %(total_frames)d' % vars()
-			scene.sequence_value = i
-			scene.camera.eye = rotate_eye(eye, target, i, rotations, row)
+			scene.camera.target = tuple(eye + vec3(pos[i]) * d)
+			if i == 4: scene.camera.camera_mode = 1 # Top Mode 真上、真下を向くときは上方向モード.
+			if i == 5: scene.camera.bank = rot + math.pi
  			scene.rendering.render()
 			scene.rendering.append_animation()
-			#scene.camera.rotate_eye(rad)
 		scene.rendering.finish_animation()
 		renderinfo.total_time = time.time() - renderinfo.start_time
 		renderinfo.total_frames = total_frames
 		renderinfo.average_time = float(renderinfo.total_time) / float(total_frames)
 	finally:
 		# レンダリング前の設定を復帰.
+		scene.rendering.image_size = saved_image_size
 		animation_settings.step = saved_step
 		animation_settings.starting_frame = saved_starting_frame
 		animation_settings.ending_frame = saved_ending_frame
 		animation_settings.object_movie_mode = saved_object_movie_mode
-		scene.camera.eye = saved_eye
+		scene.camera.target = saved_target
+		scene.camera.zoom = saved_zoom
+		scene.camera.bank = saved_bank
+		scene.camera.camera_mode = saved_camera_mode
 		scene.active_shapes = saved_active_shapes
-		scene.sequence_value = saved_sequence_value
 
 def version_info ():
 	try:
@@ -269,6 +284,10 @@ def version_info ():
 		return 'Shade 10 (%d)' % xshade.shade().version
 
 def write_index_html (index_path):
+	width = renderinfo.image_size[0]
+	height = renderinfo.image_size[1]
+	depth = max(width, height) / 2 - 1
+	pers = 2 * max(width, height) / 3
 	f = open(index_path, "w")
 	f.write("""<!DOCTYPE html>
 <html>
@@ -276,7 +295,28 @@ def write_index_html (index_path):
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=1024; maximum-scale=1.2;" />
 	<meta name="generator" content=\"""" + version_info() + """\">
-	<title>HTML5 Object VR</title>
+	<title>HTML5 Cubic VR</title>
+	<style>
+		#controller { """ + "position:absolute; z-index:100; width:%dpx; height:%dpx; top:0px; left:0;" % (width, height) + """}
+		#viewer { """ + "position:absolute; width:%dpx; height:%dpx; overflow:hidden;" % (width, height) + """
+			""" + "-webkit-perspective:%d; -webkit-transform-style:preserve-3d;" % (pers) + """
+			""" + "-moz-perspective:%d; -moz-transform-style:preserve-3d;" % (pers) + """
+		}
+		#cube { """ + "width:%dpx; height:%dpx;" % (width, height) + """
+			""" + "-webkit-transform-style:preserve-3d; -webkit-transform:translateZ(%dpx);" % (pers) + """
+			""" + "-moz-transform-style:preserve-3d; -moz-transform:translateZ(%dpx);" % (pers) + """
+		}
+		.side { position:absolute; left:0px; top:0px;
+			-webkit-backface-visibility:hidden;
+			-moz-backface-visibility:hidden;
+		}
+		""" + "#side0 { -webkit-transform:translateZ(-%dpx); -moz-transform:translateZ(-%dpx); }" % (depth, depth) + """
+		""" + "#side1 { -webkit-transform:rotateY(-90deg) translateZ(-%dpx); -moz-transform:rotateY(-90deg) translateZ(-%dpx); }" % (depth, depth) + """
+		""" + "#side2 { -webkit-transform:rotateY(180deg) translateZ(-%dpx); -moz-transform:rotateY(180deg) translateZ(-%dpx); }" % (depth, depth) + """
+		""" + "#side3 { -webkit-transform:rotateY( 90deg) translateZ(-%dpx); -moz-transform:rotateY( 90deg) translateZ(-%dpx); }" % (depth, depth) + """
+		""" + "#side4 { -webkit-transform:rotateX(-90deg) translateZ(-%dpx); -moz-transform:rotateX(-90deg) translateZ(-%dpx); }" % (depth, depth) + """
+		""" + "#side5 { -webkit-transform:rotateX( 90deg) translateZ(-%dpx); -moz-transform:rotateX( 90deg) translateZ(-%dpx); }" % (depth, depth) + """
+	</style>
 	<script>
 		function addEventListener_ (obj, type, listener, useCapture) {
 			if (obj.addEventListener) { obj.addEventListener(type, listener, useCapture); } // all browsers, except IE.
@@ -294,102 +334,96 @@ def write_index_html (index_path):
 			if (event.preventDefault) { event.preventDefault(); }
 			else { event.returnValue = false; }
 		}
-		objectvr = {
-			imageName: function (name, num, ext, index) {
-				m = (num + index)
-				return name + m.substr(m.length - num.length) + ext;
-			},
+		cubicvr = {
 			rotate: function (e) {
-				var deltaX = parseInt((objectvr._firstX - e.clientX) / objectvr._stepX);
-				var deltaY = parseInt((objectvr._firstY - e.clientY) / objectvr._stepY);
-				objectvr._X = objectvr._lastX + deltaX;
-				objectvr._Y = objectvr._lastY + deltaY;
-				if (0 <= objectvr._X) { objectvr._X = (objectvr._X % (objectvr._cols - 1)); }
-				else                  { objectvr._X = (objectvr._cols - 1) - ((Math.abs(objectvr._X) - 1) % (objectvr._cols - 1)); }
-				objectvr._Y = Math.max(Math.min(objectvr._Y, objectvr._rows - 1), 0);
-				var index = objectvr._X + objectvr._Y * objectvr._cols;
-				index = Math.max(Math.min(index, objectvr._N - 1), 0);
-				objectvr._img.src = objectvr.imageName(objectvr._name, objectvr._num, objectvr._ext, index);
+				var deltaX = parseInt((cubicvr._firstX - e.clientX) / cubicvr._step);
+				var deltaY = parseInt((cubicvr._firstY - e.clientY) / cubicvr._step);
+				cubicvr._X = cubicvr._lastX + deltaX;
+				cubicvr._Y = cubicvr._lastY + deltaY;
+				cubicvr._Y = Math.max(-88, Math.min(88, cubicvr._Y));
+				""" + "cubicvr._cube.style.webkitTransform = 'translateZ(%dpx) rotateX(' + cubicvr._Y + 'deg) rotateY(' + cubicvr._X + 'deg)';" % (pers) + """
+				""" + "cubicvr._cube.style.mozTransform = 'translateZ(%dpx) rotateX(' + cubicvr._Y + 'deg) rotateY(' + cubicvr._X + 'deg)';" % (pers) + """
 				removeSelection_();
 			},
 			mousemove: function(e) {
-				objectvr.rotate(e);
+				cubicvr.rotate(e);
 				preventDefault_(e);
 			},
 			touchmove: function(e) {
-				objectvr.rotate(e.touches[0]);
+				cubicvr.rotate(e.touches[0]);
 				preventDefault_(e);
 			},
-			init: function(cols, rows, stepX, stepY, name, num, ext) {
-				var frames = cols * rows;
-				for (var i = 0; i < frames; ++i) {
-					var imgObj = new Image();
-					imgObj.src = this.imageName(name, num, ext, i);
-				}
-				this._name = name;
-				this._num = num;
-				this._ext = ext;
-				this._cols = cols;
-				this._rows = rows;
-				this._N = frames;
-				this._stepX = stepX;
-				this._stepY = stepY;
+			mousedown: function(e) {
+				cubicvr._firstX = e.clientX;
+				cubicvr._firstY = e.clientY;
+				addEventListener_(document, 'mousemove', cubicvr.mousemove, true);
+				preventDefault_(e);
+			},
+			touchstart: function(e) {
+				cubicvr._firstX = e.touches[0].clientX;
+				cubicvr._firstY = e.touches[0].clientY;
+				addEventListener_(document, 'touchmove', cubicvr.touchmove, true);
+				preventDefault_(e);
+			},
+			init: function(step) {
+				this._step = step;
 				this._firstX = 0;
 				this._firstY = 0;
-				this._X = cols - 1;
-				this._Y = rows - 1;
+				this._X = 0;
+				this._Y = 0;
 				this._lastX = this._X;
 				this._lastY = this._Y;
-				this._viewer = document.getElementById('viewer');
-				this._viewer.innerHTML = '';
-				this._img = document.createElement("img");
-				this._img.id = 'objvr';
-				var index = objectvr._X + objectvr._Y * objectvr._cols;
-				index = Math.max(Math.min(index, objectvr._N - 1), 0);
-				this._img.src = this.imageName(name, num, ext, index);
-				this._viewer.appendChild(this._img);
-				addEventListener_(this._img, 'mousedown', function mousedown(e) {
-					objectvr._firstX = e.clientX;
-					objectvr._firstY = e.clientY;
-					addEventListener_(document, 'mousemove', objectvr.mousemove, true);
-					preventDefault_(e);
-				}, true);
+				this._cube = document.getElementById('cube');
+				this._controller = document.getElementById('controller');
+				for (var i = 0; i < 6; ++i) {
+					addEventListener_(document.getElementById('side' + i), 'mousedown', this.mousedown, true);
+					addEventListener_(document.getElementById('side' + i), 'touchstart', this.touchstart, true);
+				}
+				addEventListener_(this._controller, 'mousedown', this.mousedown, true);
 				addEventListener_(document, 'mouseup', function mouseup(e) {
-					removeEventListener_(document, 'mousemove', objectvr.mousemove, true);
-					objectvr._lastX = objectvr._X;
-					objectvr._lastY = objectvr._Y;
+					removeEventListener_(document, 'mousemove', cubicvr.mousemove, true);
+					cubicvr._lastX = cubicvr._X;
+					cubicvr._lastY = cubicvr._Y;
 				}, true);
-				addEventListener_(this._img, 'touchstart', function touchstart (e) {
-					objectvr._firstX = e.touches[0].clientX;
-					objectvr._firstY = e.touches[0].clientY;
-					addEventListener_(document, 'touchmove', objectvr.touchmove, true);
-					preventDefault_(e);
-				}, true);
+				addEventListener_(this._controller, 'touchstart', this.touchstart, true);
 				addEventListener_(document, 'touchend', function touchend (e) {
-					removeEventListener_(document, 'touchmove', objectvr.touchmove, true);
-					objectvr._lastX = objectvr._X;
-					objectvr._lastY = objectvr._Y;
+					removeEventListener_(document, 'touchmove', cubicvr.touchmove, true);
+					cubicvr._lastX = cubicvr._X;
+					cubicvr._lastY = cubicvr._Y;
 				}, true);
+				this._loading = document.getElementById('loading');
+				this._loading.innerHTML = '';
 			}
 		};
 		window.onload = function () {
-			objectvr.init(""" + ("%d, %d, %d, %d, 'images/objvr', '0000', '%s'" % (settings.number_of_col, settings.number_of_row, stepX(90), stepY(90), output_extension())) + """);
+			cubicvr.init(3);
 		}
 	</script>
 </head>
 <body>
 	<header>
-		<h1>HTML5 Object VR</h1>
+		<h1>HTML5 Cubic VR</h1>
 	</header>
-	<section id="objectvr-main" style="width:""" + ('%d' % renderinfo.image_size[0]) + """px; margin:0 auto 0 auto;">
-		<div id="viewer">Loading...</div>
-		<section id="rendering_info">
-			<p>Rendering Info</p>
-			""" + version_info() + """<br>
-			Total Time: """ + "%d sec." % renderinfo.total_time + """<br>
-			Total Frames: """ + "%d" % renderinfo.total_frames + """<br>
-			Average Time: """ + "%.2f sec." % renderinfo.average_time + """<br>
-		</section>
+	<section id="cubicvr-main" style=" """ + "width:%dpx; height:%dpx; margin:0 auto 0 auto;" % (width, height)+ """ ">
+		<div id="viewer">
+			<div id="cube">
+				<img id="side0" class="side" src="images/cubicvr0000""" + output_extension() + """"/>
+				<img id="side1" class="side" src="images/cubicvr0001""" + output_extension() + """"/> 
+				<img id="side2" class="side" src="images/cubicvr0002""" + output_extension() + """"/>
+				<img id="side3" class="side" src="images/cubicvr0003""" + output_extension() + """"/>
+				<img id="side4" class="side" src="images/cubicvr0004""" + output_extension() + """"/>
+				<img id="side5" class="side" src="images/cubicvr0005""" + output_extension() + """"/>
+			</div>
+			<div id="controller"></div>
+			<div id="loading">Loading...</div>
+		</div>
+	</section>
+	<section id="rendering_info">
+		<p>Rendering Info</p>
+		""" + version_info() + """<br>
+		Total Time: """ + "%d sec." % renderinfo.total_time + """<br>
+		Total Frames: """ + "%d" % renderinfo.total_frames + """<br>
+		Average Time: """ + "%.2f sec." % renderinfo.average_time + """<br>
 	</section>
 </body>
 </html>""")
@@ -404,7 +438,7 @@ if open_option_dialog():
 	images_path = os.path.join(settings.output_path, decode('images'))
 	if not os.path.exists(images_path):
 		os.mkdir(images_path)
-	file_path = os.path.join(images_path, decode('objvr' + output_extension()))
+	file_path = os.path.join(images_path, decode('cubicvr' + output_extension()))
 	start_rendering(scene, file_path)
 	index_path = os.path.join(settings.output_path, decode('index.html'))
 	write_index_html(index_path)
